d45c847e7ae318c04bde15d2ff6b45a699aec707
DrowningToast <supratouchgussii@gmail.com>
2023-05-29 09:54:24 +0700

    add: react hook form

--- src/components/CreatePostForm.jsx
+++ src/components/CreatePostForm.jsx
@@ -1,36 +1,40 @@
 import { useRef } from "react";
 import { pbClient } from "../api/pocketbase";
+import { useForm } from "react-hook-form";
 
 const CreatePostForm = () => {
-  const titleField = useRef(null);
-  const contentField = useRef(null);
+  const { register, handleSubmit } = useForm();
 
-  const handleCreatePost = async () => {
-    const title = titleField.current.value;
-    const content = contentField.current.value;
-    pbClient.collection("posts").create({
-      title,
-      content,
-      username: "Unknown",
-    });
-    alert("Post created");
+  const handleCreatePost = async (data) => {
+    pbClient
+      .collection("posts")
+      .create({
+        title: data.title,
+        data: data.content,
+        username: "Unknown",
+      })
+      .then((res) => {
+        console.log(data);
+        alert("Post created");
+      });
   };
 
   return (
-    <div className="flex flex-col gap-y-2 w-full">
+    <form
+      onSubmit={handleSubmit(handleCreatePost)}
+      className="flex flex-col gap-y-2 w-full"
+    >
       <label htmlFor="title" className="text-white">
         New Post
       </label>
-      <input className="w-full" type="text" ref={titleField} />
-      <textarea className="w-full" ref={contentField} />
-      <button
-        onClick={handleCreatePost}
+      <input {...register("title")} className="w-full" type="text" />
+      <textarea {...register("content")} className="w-full" />
+      <input
         className="border-2 border-white text-white px-8 w-full"
-        type="button"
-      >
-        Post
-      </button>
-    </div>
+        type="submit"
+        value="Post"
+      ></input>
+    </form>
   );
 };
 
